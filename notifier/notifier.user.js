e8f43b9bfc036957347a36d9c6be3c7ed01da22b
tttooottt <66778352+tttooottt@users.noreply.github.com>
2020-09-15 18:10:57 +0300

    Code cleanup

--- notifier/notifier.user.js
+++ notifier/notifier.user.js
@@ -27,7 +27,7 @@ const RC = new ESORoomChecker;
 var currentNode;
 
 function sendData(details) {
-    const actorId = details.id || details.sender || details.author; //backend developer is brainlet
+    const actorId = details.id || details.sender || details.author; 
 
     const _data = {
         "code": details.reason,
