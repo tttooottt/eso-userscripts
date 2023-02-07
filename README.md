9105f4ad86b57367bcb17860a034faaab633a91e
tttooottt <tttooo.ttt@mail.ru>
2023-02-07 15:40:18 +0300

    Tidy up

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
 # eso-userscripts
-Usercripts repository for ESO
-# List of available userscripts
-## ESO Console Message Logging
-Log textual messages in console
+Usercripts for ESO
+# Userscripts
 
-[Install](https://github.com/tttooottt/eso-userscripts/raw/master/console-message-logging/console-message-logging.user.js)
-## ESO Title Message
-> **WARNING** This userscript is considered DEPRECATED. It is now supported by ESO itself
-
-Display current amount of messages in chat in title(tab)
-
-## ESO Notifier
-> **WARNING** This userscript is no longer supported by this repo. Instead, check out [here](https://github.com/questionableprofile/eso-userscripts)
-
-Usercript for [esnotifier](https://github.com/questionableprofile/esnotifier) support
-
-# List of available libraries(for developers)
+# Libraries(for developers)
 ## ESO Room Checker
 Get current users in room
--- libs/room-checker/room-checker.user.js
+++ libs/room-checker/room-checker.user.js
@@ -10,21 +10,22 @@
 // ==/UserScript==
 
 const RoomEventHandlers = {
-    'nodeData': (event, that) => { that.users = event.detail.users },
-    'userJoin': (event, that) => { that.users.push(event.detail.user) },
-    'userLeave': (event, that) => { that.users = that.users.filter(user => user.id != event.detail.id) }
+	'nodeData': (event, that) => { that.users = event.detail.users },
+	'userJoin': (event, that) => { that.users.push(event.detail.user) },
+	// TODO: Optimize filter
+	'userLeave': (event, that) => { that.users = that.users.filter(user => user.id != event.detail.id) }
 };
 
-class ESORoomChecker {
-    users;
-    
-    constructor() {
-        this.users = [];
-        
-        Object.keys(RoomEventHandlers).map(eventCode => {
-            document.addEventListener(eventCode, (event) => {
-                RoomEventHandlers[eventCode](event, this);
-            });
-        });
-    }
+class ESORoom {
+	users;
+
+	constructor() {
+		this.users = [];
+		
+		Object.keys(RoomEventHandlers).map(eventCode => {
+			document.addEventListener(eventCode, (event) => {
+				RoomEventHandlers[eventCode](event, this);
+			});
+		});
+	}
 }
