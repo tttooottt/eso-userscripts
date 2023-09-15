8c39471a4a2b6c117c4873b69171313e333d41e2
tttooottt <tttooo.ttt@mail.ru>
2023-09-15 03:11:32 +0300

    Add ESO Dress Nodes Unlimited

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Usercripts for ESO
 # Userscripts
 ## ESO Chat to Speech
 
+[Install](https://github.com/tttooottt/eso-userscripts/raw/master/chat-speech/chat-speech.user.js)
+## ESO Dress Nodes Unlimited
+
+[Install](https://github.com/tttooottt/eso-userscripts/raw/master/dress-nodes/dress-nodes.user.js)
 # Libraries(for developers)
 ## ESO Room Checker
 Get current users in room
---
+++ dress-nodes/dress-nodes.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        ESO Dress Nodes Unlimited
+// @namespace   tttooottt
+// @version     0.1.0
+// @author      tttooottt
+// @description Dress in any node
+// @match       *://*.esonline.tk/
+// @grant       unsafeWindow
+// @updateURL   https://github.com/tttooottt/eso-userscripts/raw/master/dress-nodes/dress-nodes.user.js
+// @downloadURL https://github.com/tttooottt/eso-userscripts/raw/master/dress-nodes/dress-nodes.user.js
+// @license     MIT
+// ==/UserScript==
+
+const ConnectEvent = 'serverTimecode';
+
+function initMod() {
+	fetch(unsafeWindow.esoConfig.href + '/nodes')
+	.then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		let nodes = [];
+		for (const node of data.nodes) {
+			nodes.push(node.code);
+		}
+		unsafeWindow.esoConfig.dressNodes = nodes;
+	}).catch(function(err) {
+		console.log(err);
+	});
+}
+
+document.addEventListener(ConnectEvent, initMod, { 'once': true });
